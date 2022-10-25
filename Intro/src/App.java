68a412556e36f7489d59b540613a6afa9b774c01
Tai Huynh <69029687+1BarCode@users.noreply.github.com>
2022-10-25 00:42:49 -0700

    init java app

---
+++ Intro/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+ * 
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int inputInt = scan.nextInt();
+        System.out.println("Your input is: " + inputInt);
+
+        scan.close();
+        
+    }
+}
+
