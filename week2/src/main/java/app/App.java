1b96dac3a8b6599021d41c2cdb12c29a03aa02c6
Tai Huynh <69029687+1BarCode@users.noreply.github.com>
2022-11-05 20:36:03 -0700

    finished arraylist

--- week2/src/main/java/app/App.java
+++ week2/src/main/java/app/App.java
@@ -1,15 +1,38 @@
 package app;
 
-import customstring.CustomString;
+import java.lang.reflect.Array;
+
+import classroom.Classroom;
+import classroom.Student;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
 
-        CustomString myCustomString = new CustomString();
-        myCustomString.setString("Some real test.");
-        String res = myCustomString.filterLetters('E', true);
-        System.out.println("==> "+ res);
-        System.out.println("Done");
+        Classroom hunstman = new Classroom("HH", "105", 10, 5);
+
+        Student finegan = new Student("finegan", "fineganw");
+        Student bob = new Student("bob", "roberts");
+
+        hunstman.addAStudent(finegan);
+        hunstman.addAStudent(bob);
+
+        hunstman.assignStudentToSeat(finegan);
+        hunstman.assignStudentToSeat(bob);
+
+        hunstman.printAllStudents();
+
+        // String[] strings = new String[3];
+        int[] primes = {2,3,4,7,13};
+
+        // int[] set;
+        // set = new int[20];
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(primes[i]);
+        }
+
+        
+        System.out.println(hunstman);
     }
 }
